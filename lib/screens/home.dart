be7e68cd525cb0018cea2f03eac9b24f89167381
viper23 <htonnycarlos@gmail.com>
2025-01-06 14:10:02 +0200

    wip on homescreen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:travel_app_ui/utils/app_styles.dart';
 
 class MyHomeWidget extends StatelessWidget {
@@ -15,14 +17,22 @@ class MyHomeWidget extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               children: [
+                Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text("Good morning", style: AppStyles.headLineStyle3,),
-                        Text("Book Tickets", style: AppStyles.headLineStyle1,),
+                        Text(
+                          "Good morning",
+                          style: AppStyles.headLineStyle3,
+                        ),
+                        Gap(5),
+                        Text(
+                          "Book Tickets",
+                          style: AppStyles.headLineStyle1,
+                        ),
                       ],
                     ),
                     Container(
@@ -32,15 +42,57 @@ class MyHomeWidget extends StatelessWidget {
                         borderRadius: BorderRadius.circular(10),
                         image: DecorationImage(
                             fit: BoxFit.cover,
-                            image: AssetImage("assets/images/ticket-logo.png")),
+                            image:
+                                AssetImage("assets/images/ticket-logo.jpeg")),
                       ),
                     )
                     // Image.asset("assets/images/ticket-logo.png"),
+                  ],
+                ),
+                Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFFF4F6FD)),
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      Icon(
+                        FluentSystemIcons.ic_fluent_search_filled,
+                        color: Color(0xFFBFC205),
+                      ),
+                      Gap(10),
+                      Text(
+                        "Search",
+                        style: AppStyles.headLineStyle4,
+                      )
+                    ],
+                  ),
+                ),
+                Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Upcomming flights",
+                      style: AppStyles.headLineStyle2,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        print("object");
+                      },
+                      child: Text(
+                        "View all",
+                        style: AppStyles.textStyle
+                            .copyWith(color: AppStyles.primaryColor),
+                      ),
+                    )
                   ],
                 )
               ],
             ),
           )
+          
         ],
       ),
     );
