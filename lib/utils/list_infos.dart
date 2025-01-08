22976e7850b6c503706dfdbaa042dcdb6f4561a3
viper23 <htonnycarlos@gmail.com>
2025-01-08 16:49:14 +0200

    database for dynamically homepage

---
+++ lib/utils/list_infos.dart
@@ -0,0 +1,51 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    "image": "hotel_1.jpg",
+    "place": "Open space",
+    "destination": "London",
+    "price": "25",
+  },
+  {
+    "image": "hotel_2.jpeg",
+    "place": "Global will",
+    "destination": "London",
+    "price": "40",
+  },
+  {
+    "image": "hotel_3.jpeg",
+    "place": "Tallest building",
+    "destination": "Dubail",
+    "price": "68",
+  }
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    "from": {
+      "code": "NYC",
+      "name": "New york",
+    },
+    "to": {
+      "code": "LDN",
+      "name": "London",
+    },
+    "flying_time": "8H 30M",
+    "date": "9 NOV",
+    "departure_time": "08 : 00AM",
+    "number": 23,
+  },
+  {
+    "from": {
+      "code": "KGL",
+      "name": "Kigali",
+    },
+    "to": {
+      "code": "BJR",
+      "name": "Bujumbura",
+    },
+    "flying_time": "10H 10M",
+    "date": "23 JUN",
+    "departure_time": "10 : 00AM",
+    "number": 11,
+  },
+];
