a2adb1c8df8b5d936e745eb3934d1ece593d50ee
viper23 <htonnycarlos@gmail.com>
2025-01-09 10:18:53 +0200

    function to adjust width and height

--- lib/utils/app_layout.dart
+++ lib/utils/app_layout.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class AppLayout {
   static getSize(BuildContext context) {
     return MediaQuery.of(context).size;
+  }
+
+  static getScreenWidth() {
+    return Get.width;
+  }
+
+  static getScreenHeigth() {
+    return Get.height;
+  }
+
+  static getHeight(double pixels) {
+    double x = getScreenHeigth() / pixels;
+    return getScreenHeigth() / x;
+  }
+
+  static getWidht(double pixels) {
+    double x = getScreenWidth() / pixels;
+    return getScreenWidth() / x;
   }
 }
